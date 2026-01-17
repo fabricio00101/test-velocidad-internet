0eac12b4b34c33fc648bd1bfd170a941a0a02a3e
fabricio00101 <jfmilanesio@gmail.com>
2026-01-17 09:13:22 -0300

    Cambios en la función

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import speedtest
 
 def realizar_test():
     
-    test = speedtest.Speedtest()
+    test = speedtest.Speedtest(secure=True)
 
     print('Buscando el mejor sevidor...')
 
@@ -12,12 +12,15 @@ def realizar_test():
 
     velocidad_descarga = test.download()
 
-    print('midiendo velocidad de subida...')
+    print('Midiendo velocidad de subida...')
     velocidad_subida = test.upload()
 
+    descarga_mbs = velocidad_descarga / (8 * 10**6)
+    subida_mbs = velocidad_subida / (8 * 10**6)
+
     print('\n--- RESULTADOS ---')
-    print(f'Velocidad de Descarga: {velocidad_descarga / 10**6:.2f} Mbps')
-    print(f'Velocidad de subida: {velocidad_subida / 10**6:.2f} Mbps')
+    print(f'Velocidad de Descarga: {descarga_mbs:.2f} Mb/s')
+    print(f'Velocidad de subida: {subida_mbs:.2f} Mbp/s')
 
 if __name__ == '__main__':
     realizar_test()
